369395bde51a539b7a472eadfc244c9a37b98ae1
ahmd-nabil <ahm3dnabil99@gmail.com>
2024-03-09 17:15:25 +0200

    first commit

---
+++ src/main/java/nabil/awsrdbsexample/AwsRdbsExampleApplication.java
@@ -0,0 +1,13 @@
+package nabil.awsrdbsexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsRdbsExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsRdbsExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nabil/awsrdbsexample/AwsRdbsExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package nabil.awsrdbsexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsRdbsExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
